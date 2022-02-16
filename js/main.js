6ede9203be96ab778e203bdf558a70a6bf0ad2ec
kanako <87798077+kanako33333@users.noreply.github.com>
2022-02-16 22:42:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,66 @@
+"use strict";
+
+$(function() {
+
+  // メニューをhoverしたら顔が出てくる
+  //マウスを乗せたら発動
+  $(".menu-link1").hover(function() {
+
+    //マウスを乗せたら顔が出てくる
+    $(".menu-face1").css('display', 'block');
+
+  },function() {
+
+    // カーソルが離れた時の処理
+    $(".menu-face1").css('display', 'none');
+
+  });
+  //マウスを乗せたら発動
+  $(".menu-link2").hover(function() {
+
+    //マウスを乗せたら顔が出てくる
+    $(".menu-face2").css('display', 'block');
+
+  },function() {
+
+    // カーソルが離れた時の処理
+    $(".menu-face2").css('display', 'none');
+
+  });
+  //マウスを乗せたら発動
+  $(".menu-link3").hover(function() {
+
+    //マウスを乗せたら顔が出てくる
+    $(".menu-face3").css('display', 'block');
+
+  },function() {
+
+    // カーソルが離れた時の処理
+    $(".menu-face3").css('display', 'none');
+
+  });
+  //マウスを乗せたら発動
+  $(".menu-link4").hover(function() {
+
+    //マウスを乗せたら顔が出てくる
+    $(".menu-face4").css('display', 'block');
+
+  },function() {
+
+    // カーソルが離れた時の処理
+    $(".menu-face4").css('display', 'none');
+
+  });
+  //マウスを乗せたら発動
+  $(".menu-link5").hover(function() {
+
+    //マウスを乗せたら顔が出てくる
+    $(".menu-face5").css('display', 'block');
+
+  },function() {
+
+    // カーソルが離れた時の処理
+    $(".menu-face5").css('display', 'none');
+
+  });
+});
